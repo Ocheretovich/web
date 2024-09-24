c7682a0e06d260c92e9d6207150df2cb1dcd50af
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-09-25 09:44:16 +1000

    feat: use shapeshift api proxy for 0x swapper

--- src/config.ts
+++ src/config.ts
@@ -175,6 +175,7 @@ const validators = {
   REACT_APP_PORTALS_BASE_URL: url(),
   REACT_APP_ZERION_BASE_URL: url(),
   REACT_APP_FEATURE_PHANTOM_WALLET: bool({ default: false }),
+  REACT_APP_ZRX_BASE_URL: url(),
 }
 
 function reporter<T>({ errors }: envalid.ReporterOptions<T>) {
