641cfe251496a9a6302d260ced6f9287f19000b8
woodenfurniture <125113430+woodenfurniture@users.noreply.github.com>
2024-10-02 08:50:14 +1000

    fix: 0x trades from native assets

--- src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/helpers.ts
+++ src/components/MultiHopTrade/components/MultiHopTradeConfirm/hooks/helpers.ts
@@ -1,5 +1,6 @@
 import type { HDWallet } from '@shapeshiftoss/hdwallet-core'
 import { SwapperName, type TradeQuote, type TradeQuoteStep } from '@shapeshiftoss/swapper'
+import { isNativeEvmAsset } from '@shapeshiftoss/swapper/dist/swappers/utils/helpers/helpers'
 import { getConfig } from 'config'
 import { type AllowanceType, getApprovalAmountCryptoBaseUnit } from 'hooks/queries/useApprovalFees'
 import { approve } from 'lib/utils/evm/approve'
@@ -31,6 +32,8 @@ export const approveTrade = async ({
 }
 
 export const isPermit2Hop = (tradeQuoteStep: TradeQuoteStep | undefined) => {
+  if (!tradeQuoteStep) return false
   const isPermit2Enabled = getConfig().REACT_APP_FEATURE_ZRX_PERMIT2
-  return isPermit2Enabled && tradeQuoteStep?.source === SwapperName.Zrx
+  const isNativeSellAsset = isNativeEvmAsset(tradeQuoteStep.sellAsset.assetId)
+  return isPermit2Enabled && tradeQuoteStep.source === SwapperName.Zrx && !isNativeSellAsset
 }
